56404ed08778b3fe60ecde4e05d036c3e4b64a8e
Sai Madhur CH <saimadhurch@gmail.com>
2022-03-14 02:41:23 +0530

    Changes made to ChatService

--- server/chatservice.go
+++ server/chatservice.go
@@ -3,6 +3,7 @@ package server
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/Sai-Madhur-CH/gRPC-Master/proto/chatpb"
 )
@@ -15,6 +16,7 @@ type ChatServer struct {
 func (s *ChatServer) Send(c context.Context, req *chatpb.ChatRequest) (*chatpb.ChatResponse, error) {
 	fmt.Printf("Send Function us invoked with :%v \n", req)
 	msg := req.GetReq()
+	msg.Date = time.Now().Format(time.RFC1123)
 	res := &chatpb.ChatResponse{Res: msg}
 	return res, nil
 }
